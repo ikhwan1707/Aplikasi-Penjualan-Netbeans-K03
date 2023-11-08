d8cccca8dcd748a707d6324545fb75b8e726ce07
naylazahra27 <naylazahra.tugas@gmail.com>
2023-11-08 14:51:06 +0700

    bikin barangmasuk ananda

---
+++ src/aplikasi/penjualan/netbeans/k03/ResultSets.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi.penjualan.netbeans.k03;
+
+/**
+ *
+ * @author SMKI Utama9
+ */
+class ResultSets {
+    
+}
